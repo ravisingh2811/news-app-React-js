8d4987ace4ed08d802c42d9cad9877e53f9386ab
Ravi Kumar Singh <rk250020@gmail.com>
2023-02-03 23:48:30 +0530

    adding category in navbar and author, date, and adding updateNes fuction

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   
   render() {
     return (
+      <BrowserRouter>
       <div>
          <Navbar/>
-      <News pageSize="9"/>
-      </div>
+         <Routes>
+         <Route exact  path="/" element={ <News key="general"  pageSize={9} country="in" category="general"/>} />
+         <Route exact path="/technology" element={ <News key = "tec" pageSize={9} country="in" category="technology"/>} />
+         <Route exact path="/sports" element={ <News key = "spo" pageSize={9} country="in" category="sports"/>} />
+         <Route exact path="/science" element={<News key = "science" pageSize={9} country="in" category="science"/>} />
+         <Route exact path="/health" element={ <News key = "heal" pageSize={9} country="in" category="health"/>} />
+         <Route exact path="/business" element={ <News key = "bu" pageSize={9} country="in" category="business"/>} />
+         <Route exact path="/entertainment" element={ <News  key = "en" pageSize={9} country="in" category="entertainment"/>} />
+
+         </Routes>
       
+      </div>
+      </BrowserRouter>
     )
   }
 }
